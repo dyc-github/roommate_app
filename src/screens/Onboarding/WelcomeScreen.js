3dabab00f8b1ca635338393ba5d53a84ef02998a
David Choi <45525219+dyc-github@users.noreply.github.com>
2023-01-04 15:02:37 -0800

    updated signup and welcome

--- src/screens/Onboarding/WelcomeScreen.js
+++ src/screens/Onboarding/WelcomeScreen.js
@@ -1,10 +1,12 @@
 import "react-native-gesture-handler";
-import { View } from "react-native";
-import { Button } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Button, useTheme } from "react-native-paper";
 
 export default function WelcomeScreen({navigation}){
+    const theme = useTheme();
     return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
+        <View style = {[styles.quarterCircle,{backgroundColor: theme.colors.secondaryContainer}]}/>
         <Button mode="contained" onPress={()=>{
             navigation.navigate("LoginScreen");
         }}>Login</Button>
@@ -13,3 +15,13 @@ export default function WelcomeScreen({navigation}){
         }}>Sign up</Button>
     </View>)
 }
+const styles = StyleSheet.create({
+    quarterCircle: {
+        position: "absolute",
+        width: 270,
+        height: 270,
+        borderTopLeftRadius: 640 / 2,
+        bottom: 0,
+        right: 0,
+      },
+})
